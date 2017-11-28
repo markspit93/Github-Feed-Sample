5bb5e9caf74b0ece9cd35b8b0bcad5b35ce2cfb2
Mark Spiteri <spiterimark1993@gmail.com>
2017-11-28 10:27:46 +0100

    Experimented with StrictMode

--- app/src/main/kotlin/com/github/feed/sample/App.kt
+++ app/src/main/kotlin/com/github/feed/sample/App.kt
@@ -2,6 +2,7 @@ package com.github.feed.sample
 
 import android.app.Activity
 import android.app.Application
+import android.os.StrictMode
 import com.facebook.stetho.Stetho
 import com.github.feed.sample.di.DaggerAppComponent
 import dagger.android.AndroidInjector
@@ -15,12 +16,24 @@ class App : Application(), HasActivityInjector {
 
     override fun onCreate() {
         super.onCreate()
+        initDagger()
+        initStetho()
+//        initStrictMode()
+    }
+
+    override fun activityInjector(): AndroidInjector<Activity> {
+        return activityInjector
+    }
+
+    private fun initDagger() {
         DaggerAppComponent
                 .builder()
                 .application(this)
                 .build()
                 .inject(this)
+    }
 
+    private fun initStetho() {
         if (BuildConfig.DEBUG_MODE) {
             Stetho.initialize(Stetho.newInitializerBuilder(this)
                     .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
@@ -29,7 +42,17 @@ class App : Application(), HasActivityInjector {
         }
     }
 
-    override fun activityInjector(): AndroidInjector<Activity> {
-        return activityInjector
+    private fun initStrictMode() {
+        if (BuildConfig.DEBUG_MODE) {
+            StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build())
+
+            StrictMode.setVmPolicy(StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build())
+        }
     }
 }
